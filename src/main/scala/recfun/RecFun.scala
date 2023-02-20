f905889a85f2f4000733983d728f9957771cc651
Artur Aliakseyeu <aae@krononsoft.com>
2023-02-20 14:35:22 +0300

    RecFun

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -12,14 +12,32 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || (c == r)) 1
+    else pascal(c-1, r-1) + pascal(c, r - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean =
+    def check(chars: List[Char], count: Int): Boolean =
+      if (count<0 || ((chars.isEmpty) && (count!=0))) 
+        then false
+      else if ((chars.isEmpty) && (count==0))
+        then true
+      else if (chars.head=='(')
+        then check(chars.tail, count+1)
+      else if (chars.head==')')
+        then check(chars.tail, count-1)
+      else check(chars.tail, count)
+    check(chars, 0)
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int =
+    def check(money: Int, coins: List[Int]): Int =
+      if (coins.isEmpty || money < 0) 0
+      else if (money == 0) 1
+      else check(money, coins.tail) + check(money - coins.head, coins)
+    check(money, coins)
