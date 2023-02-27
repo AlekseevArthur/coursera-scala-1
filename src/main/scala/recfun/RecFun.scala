d0951f9fe40e7a003db796deb472a8c664d79d8d
Artur Aliakseyeu <aae@krononsoft.com>
2023-02-27 13:20:09 +0300

    Refactoring

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -1,39 +1,46 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object RecFun extends RecFunInterface:
 
   def main(args: Array[String]): Unit =
     println("Pascal's Triangle")
     for row <- 0 to 10 do
-      for col <- 0 to row do
-        print(s"${pascal(col, row)} ")
+      for col <- 0 to row do print(s"${pascal(col, row)} ")
       println()
 
-  /**
-   * Exercise 1
-   */
+  /** Exercise 1
+    */
   def pascal(c: Int, r: Int): Int =
-    if (c == 0 || (c == r)) 1
-    else pascal(c-1, r-1) + pascal(c, r-1)
+    if (c == 0 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
 
-  /**
-   * Exercise 2
-   */
+  /** Exercise 2
+    */
   def balance(chars: List[Char]): Boolean =
+    @tailrec
     def check(chars: List[Char], count: Int): Boolean =
-      if (count<0 || ((chars.isEmpty) && (count!=0))) false
-      else if ((chars.isEmpty) && (count==0)) true
-      else if (chars.head=='(') check(chars.tail, count+1)
-      else if (chars.head==')') check(chars.tail, count-1)
-      else check(chars.tail, count)
+      if (count < 0 || chars.isEmpty && count != 0)
+        false
+      else if (chars.isEmpty && count == 0)
+        true
+      else if (chars.head == '(')
+        check(chars.tail, count + 1)
+      else if (chars.head == ')')
+        check(chars.tail, count - 1)
+      else
+        check(chars.tail, count)
     check(chars, 0)
 
-  /**
-   * Exercise 3
-   */
+  /** Exercise 3
+    */
   def countChange(money: Int, coins: List[Int]): Int =
-    def check(money: Int, coins: List[Int]): Int =
-      if (coins.isEmpty || money < 0) 0
-      else if (money == 0) 1
-      else check(money, coins.tail) + check(money-coins.head, coins)
-    check(money, coins)
+    if (coins.isEmpty || money < 0)
+      0
+    else if (money == 0)
+      1
+    else
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
